a9e76d395f11e4e6fb4011ff1c7e2130a2f37c5f
Lee-Scott <leehscott24@gmail.com>
2025-02-03 10:53:11 -0800

    starting JWTService with modern Java impl (functional)

---
+++ SecureDocAIBackend/src/main/java/com/familyFirstSoftware/SecureDocAIBackend/service/JwtService.java
@@ -0,0 +1,11 @@
+package com.familyFirstSoftware.SecureDocAIBackend.service;
+
+/**
+ * @author Lee Scott
+ * @version 1.0
+ * @license FamilyFirstSoftware, LLC (<a href="https://www.FamilyFirstSoftware.com"> FFS, LLC</a>)
+ * @email FamilyFirstSoftware@gmail.com
+ * @since 1/30/2025
+ */
+public interface JwtService {
+}
