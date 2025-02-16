5fc484117054a46ed6a51fd5c992193413d45a5c
Lee-Scott <leehscott24@gmail.com>
2025-02-16 15:24:23 -0800

    Exception Handlers added

---
+++ SecureDocAIBackend/src/main/java/com/familyFirstSoftware/SecureDocAIBackend/handler/ApiAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.familyFirstSoftware.SecureDocAIBackend.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static com.familyFirstSoftware.SecureDocAIBackend.utils.RequestUtils.handleErrorResponse;
+
+/**
+ * @author Lee Scott
+ * @version 1.0
+ * @license FamilyFirstSoftware, LLC (<a href="https://www.FamilyFirstSoftware.com"> FFS, LLC</a>)
+ * @email FamilyFirstSoftware@gmail.com
+ * @since 2/16/2025
+ */
+
+@Component
+public class ApiAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        handleErrorResponse(request, response, authException);
+    }
+}
+
