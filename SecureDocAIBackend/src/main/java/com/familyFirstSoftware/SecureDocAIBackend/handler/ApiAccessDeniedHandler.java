6059a11e53b0a25f15590192f8d45187b6cda9b8
Lee-Scott <leehscott24@gmail.com>
2025-02-16 15:24:23 -0800

    Exception Handlers added

---
+++ SecureDocAIBackend/src/main/java/com/familyFirstSoftware/SecureDocAIBackend/handler/ApiAccessDeniedHandler.java
@@ -0,0 +1,31 @@
+package com.familyFirstSoftware.SecureDocAIBackend.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static com.familyFirstSoftware.SecureDocAIBackend.utils.RequestUtils.handleErrorResponse;
+
+/**
+ * @author Lee Scott
+ * @version 1.0
+ * @license FamilyFirstSoftware, LLC (<a href="https://www.FamilyFirstSoftware.com"> FFS, LLC</a>)
+ * @email FamilyFirstSoftware@gmail.com
+ * @since 2/16/2025
+ * '
+ *  Todo: add some custom logic
+ */
+
+@Component
+public class ApiAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        handleErrorResponse(request, response, accessDeniedException);
+    }
+}
+
