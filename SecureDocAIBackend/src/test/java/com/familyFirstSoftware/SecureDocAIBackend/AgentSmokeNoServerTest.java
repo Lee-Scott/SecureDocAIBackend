c33207c1a79824b532e78074fbf84f8652a71229
Lee-Scott <leehscott24@gmail.com>
2025-08-12 21:14:02 -0700

    agent:tests - add AgentSmokeNoServerTest (no server) for discovery

---
+++ SecureDocAIBackend/src/test/java/com/familyFirstSoftware/SecureDocAIBackend/AgentSmokeNoServerTest.java
@@ -0,0 +1,5 @@
+package com.familyFirstSoftware.SecureDocAIBackend;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class AgentSmokeNoServerTest { @Test void contextLoads() {} }
