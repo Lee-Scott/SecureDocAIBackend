7ff69dc3d49a6f9f5695a974d3371233466afcfd
Lee Scott <leehscott24@gmail.com>
2026-03-12 15:42:47 -0700

    stop tracking .env file

---
+++ SecureDocAIBackend/src/main/java/com/familyFirstSoftware/SecureDocAIBackend/dtorequest/PatchDocRequest.java
@@ -0,0 +1,21 @@
+package com.familyFirstSoftware.SecureDocAIBackend.dtorequest.questionnaire;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Lee Scott
+ * @version 1.0
+ * @license FamilyFirstSoftware, LLC (<a href="https://www.FamilyFirstSoftware.com"> FFS, LLC</a>)
+ * @email FamilyFirstSoftware@gmail.com
+ * @since 5/24/2025
+ */
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PatchDocRequest {
+    private String documentId;
+    private String name;
+    private String description;
+}
